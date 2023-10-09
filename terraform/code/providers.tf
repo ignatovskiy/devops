96570f2cbdd02d2a96aa0c84bce99186ee7add5f
ignatovskiy <42871833+ignatovskiy@users.noreply.github.com>
2023-10-10 02:48:49 +0300

    Add files via upload

--- terraform/code/providers.tf
+++ terraform/code/providers.tf
@@ -1,10 +1,26 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source = "yandex-cloud/yandex",
+      version = ">=0.95.0"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "<=2.2.0"
     }
   }
   required_version = ">=0.13"
+
+  backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket = "tfstate-develop-ignatovsky"
+    region = "ru-central1"
+    key = "terraform.tfstate"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    dynamodb_table = "tflock-develop"
+    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gc3746j3o6s295l6ai/etnpl780gpu6dgfs27g2"
+}
 }
 
 provider "yandex" {
