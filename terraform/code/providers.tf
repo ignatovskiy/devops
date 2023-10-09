11ff34bef9083c2e7abd391b6ddf8423bc770128
ignatovskiy <42871833+ignatovskiy@users.noreply.github.com>
2023-10-10 02:51:40 +0300

    Add files via upload

--- terraform/code/providers.tf
+++ terraform/code/providers.tf
@@ -1,7 +1,12 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source = "yandex-cloud/yandex",
+      version = ">=0.95.0"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "<=2.2.0"
     }
   }
   required_version = ">=0.13"
